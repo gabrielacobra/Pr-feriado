20c4e0e789a1897809767593ec0c4c8a0114ba9c
Gabriela Lima <gabicobra@gmail.com>
2017-10-11 21:55:05 -0300

    Manipulacao do excel

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Microsoft.Office.Interop.Excel;
 
 namespace arquivocsv
 {
@@ -7,6 +8,11 @@ namespace arquivocsv
     {
         static void Main(string[] args)
         {
+            Application ex = new Application();
+            ex.Visible = true;
+            ex.Workbooks.Add();
+
+            /* 
             string nome,email;
             int idade;
 
@@ -18,22 +24,25 @@ namespace arquivocsv
 
             Console.WriteLine("Digite a sua idade: ");
             idade = Int16.Parse(Console.ReadLine());
+            */
 
-            FileInfo fi = new FileInfo("dados_cabecalho.csv");
+            // FileInfo fi = new FileInfo("dados_cabecalho.csv");
                      
-            StreamWriter arquivo;
+            // StreamWriter arquivo;
             
-            if(fi.Exists) {
-            arquivo = new StreamWriter("dados_cabecalho.csv",true);
-            arquivo.WriteLine(nome+";"+email+";"+idade+";"+DateTime.Now.ToShortDateString()); //se ele existe, não é necessário criar o cabecalho
-            }
-            else{
-            arquivo = new StreamWriter("dados_cabecalho.csv",true); //new StreamWriter gera um objeto.
-            arquivo.WriteLine("Nome;Email;Idade;Data de Cadastro"); //se o arquivo não existe, o comando é para criar o cabecalho
-            arquivo.WriteLine(nome+";"+email+";"+idade+";"+DateTime.Now.ToShortDateString());
-            }
+            // if(fi.Exists) {
+            // arquivo = new StreamWriter("dados_cabecalho.csv",true);
+            // arquivo.WriteLine(nome+";"+email+";"+idade+";"+DateTime.Now.ToShortDateString()); //se ele existe, não é necessário criar o cabecalho
+            // }
+            // else    {
+            // arquivo = new StreamWriter("dados_cabecalho.csv",true); //new StreamWriter gera um objeto.
+            // arquivo.WriteLine("Nome;Email;Idade;Data de Cadastro"); //se o arquivo não existe, o comando é para criar o cabecalho
+            // arquivo.WriteLine(nome+";"+email+";"+idade+";"+DateTime.Now.ToShortDateString());
+            // }
                         
-            arquivo.Close();
+            // arquivo.Close();
+
+
             
         }
     }
